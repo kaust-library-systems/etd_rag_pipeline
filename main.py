ab9b10b3fbcfa3e332e2c23a776a5b85abcd06b0
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-11-23 13:33:42 +0000

    first test with langchain translating to spanish

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from langchain_ollama import ChatOllama
+
 def main():
     print("Hello from etd-rag-pipeline!")
+
+    llm = ChatOllama(
+        model="granite4:3b",
+        temperature=0,
+    )
+
+    message = [
+        (
+            "system",
+            "You are a helpful assistant that translates English to Spanish. Translate the user sentence.",
+        ),
+        (
+            "human",
+            "I love programming."
+        ),
+    ]
+
+
+    ai_msg = llm.invoke(message)
+    print(ai_msg.content)
 
 
 if __name__ == "__main__":
