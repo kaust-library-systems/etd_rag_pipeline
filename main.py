60c38b46317bd229a4b6e4cbd6ae383f29570304
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-11-29 17:24:29 +0000

    Horrible code but created the Milvus db

--- main.py
+++ main.py
@@ -1,24 +1,78 @@
+from pathlib import Path
+
 from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
+from langchain_docling import DoclingLoader
+from docling.chunking import HybridChunker
+from langchain_core.prompts import PromptTemplate
 from langchain.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_docling.loader import ExportType
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_milvus import Milvus
+from docling_core.transforms.chunker.tokenizer.huggingface import HuggingFaceTokenizer
+
+from transformers import AutoTokenizer
+
+FILE_PATH = ["/data/ETD_rag/test/133211.pdf"]
+#EMBED_MODEL_ID = "granite-embedding"
+EMBED_MODEL_ID = "ibm-granite/granite-embedding-30m-english"
+GEN_MODEL_ID = "phi4:14b"
+EXPORT_TYPE = ExportType.DOC_CHUNKS
+MILVUS_URI = Path("/") / "data" / "ETD_rag"/ "docling.db"
+
+MAX_TOKENS = 64
 
 def main():
     print("Hello from etd-rag-pipeline!")
 
-    model = ChatOllama(
-        model="granite4:3b",
-        temperature=0,
+    tokenizer = HuggingFaceTokenizer(
+        tokenizer=AutoTokenizer.from_pretrained(EMBED_MODEL_ID),
+        max_tokens=MAX_TOKENS,  # optional, by default derived from `tokenizer` for HF case
     )
 
-    system_msg = SystemMessage(
-        '''You are a helpful assistent that respond to questions with three
-           exclamation marks.'''
+    loader = DoclingLoader(
+        file_path=FILE_PATH,
+        export_type=EXPORT_TYPE,
+        chunker=HybridChunker(tokenizer=tokenizer)
     )
 
-    human_msg = HumanMessage("What is the capital of France?")
+    docs = loader.load()
 
-    answer = model.invoke([system_msg, human_msg])
-    print(answer.content)
+    if EXPORT_TYPE == ExportType.DOC_CHUNKS:
+        splits = docs
+    elif EXPORT_TYPE == ExportType.MARKDOWN:
+        from langchain_text_splitters import MarkdownHeaderTextSplitter
 
+        splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on=[
+                ("#", "Header_1"),
+                ("##", "Header_2"),
+                ("###", "Header_3"),
+            ],
+        )
+        splits = [split for doc in docs for split in splitter.split_text(doc.page_content)]
+    else:
+        raise ValueError(f"Unexpected export type: {EXPORT_TYPE}")
+
+    # Inspecting the split.
+    for d in splits[:3]:
+        print(f"- {d.page_content=}")
+        print("...")
+
+    # Ingestion
+    embedding = HuggingFaceEmbeddings(
+        model=EMBED_MODEL_ID,
+    )
+
+    vectorstore = Milvus.from_documents(
+        documents=splits,
+        embedding=embedding,
+        collection_name="docling_demo",
+        connection_args={"uri": str(MILVUS_URI)},
+        index_params={"index_type": "FLAT"},
+        drop_old=True,
+    )
 
 if __name__ == "__main__":
     main()
